0becbe13277675b9d3b59b67a651e185fb04645f
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2014-07-24 19:04:09 +0400

    httpError

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var errorhandler = require('errorhandler');
 var config = require('config');
 var log = require('lib/log')(module);
+var HttpError = require('error').HttpError;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -21,46 +23,40 @@ app.engine('ejs', require('ejs-locals')); // layout partial block
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+if (app.get('env') == 'development') {
+	app.use(logger('dev'));
+	app.use(errorhandler());
+} else {
+	app.use(logger('default'));
+}
+
 app.use(favicon());
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('middleware/sendHttpError'));
 
 app.use('/', routes);
 app.use('/users', users);
 
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+	if (typeof err == 'number') { // next(404);
+		err = new HttpError(err);
+	}
+
+	if (err instanceof HttpError) {
+		res.sendHttpError(err);
+	} else {
+		
+		if (app.get('env') == 'development') {
+			var handler = errorhandler();
+			handler(err, req, res, next);
+		} else {
+			log.error(err);
+			err = new HttpError(500);
+			res.sendHttpError(err);
+		}
+	}
 });
 
-
-//module.exports = app;
