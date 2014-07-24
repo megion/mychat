53bd8d6087546f2e1ddddc44dd2312a81078063e
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2014-07-24 10:58:48 +0400

    createDb.js

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ var nconf = require('nconf');
 var path = require('path');
 
 nconf.argv().env().file({
-	file : path.join(__dirname, 'conf.json')
+	file : path.join(__dirname, 'config.json')
 });
 
 module.exports = nconf;
