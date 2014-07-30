192501238d63496aa1736432e7fd672a9c781989
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2014-07-30 19:20:57 +0400

    create page model

---
+++ models/page.js
@@ -0,0 +1,23 @@
+var mongoose = require('lib/mongoose'),
+  Schema = mongoose.Schema,
+  ObjectId = Schema.ObjectId;
+
+var schema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String
+  },
+  parent : {
+    type : ObjectId,
+    ref: 'Page'
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+exports.Page = mongoose.model('Page', schema);
