0bf8ea993e5615b0af8775d37e5a772aa225894e
Ilya Zadorozhny <Ilya.Zadorozhny@artezio.com>
2015-03-10 17:58:49 +0300

    test3

--- routes/ngtest.js
+++ routes/ngtest.js
@@ -13,5 +13,9 @@ router.get('/', function(req, res, next) {
 router.get('/test2', function(req, res, next) {
 	res.render('ang/test2', {});
 });
+router.get('/test3', function(req, res, next) {
+	res.render('ang/test3', {});
+});
+
 
 module.exports = router;
